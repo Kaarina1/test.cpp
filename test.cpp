2ff0eccb28da130e55df689e8410d31c6c95e017
Kaarina1 <1388287@cityplym.ac.uk>
2026-01-10 12:35:14 +0000

    Add print statement for images per process

--- test.cpp
+++ test.cpp
@@ -52,6 +52,7 @@ int main(int argc, char** argv)
 		printf("There are  - %d - processes\n", number_of_processes);
 		//CALCULATE batch size = Number of images/ Number of processes
 		images_per_process = image_count/number_of_processes;
+		printf("There are  - %d - in a batch\n", images_per_process);
 		//SEND Workers Rank and batch size (MPI_Bcast)
 		MPI_Bcast(&images_per_process,1,MPI_INT,0,MPI_COMM_WORLD);
 	}
